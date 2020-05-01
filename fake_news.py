759a4ebce4dccb0d206a726f3ecf5520ef96f8b6
siva saketh <saketh.emani@gmail.com>
2020-04-30 17:02:33 -0700

    analysis py file added

--- fake_news.py
+++ fake_news.py
@@ -52,6 +52,7 @@ pac.fit(tfidf_train,y_train)
 #DataFlair - Predict on the test set and calculate accuracy
 y_pred=pac.predict(tfidf_test)
 score=accuracy_score(y_test,y_pred)
+print(y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
 
 
